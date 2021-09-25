f4b20afca603f481cd8b666b9a81802bde0703df
pablisco <260697+pablisco@users.noreply.github.com>
2021-09-26 00:41:31 +0100

    Convert Result implementation to use sealed interfaces and value classes

--- kotlin-result/src/commonMain/kotlin/com/github/michaelbull/result/Result.kt
+++ kotlin-result/src/commonMain/kotlin/com/github/michaelbull/result/Result.kt
@@ -1,5 +1,7 @@
 package com.github.michaelbull.result
 
+import kotlin.jvm.JvmInline
+
 /**
  * [Result] is a type that represents either success ([Ok]) or failure ([Err]).
  *
@@ -7,10 +9,10 @@ package com.github.michaelbull.result
  * - Haskell: [Data.Either](https://hackage.haskell.org/package/base-4.10.0.0/docs/Data-Either.html)
  * - Rust: [Result](https://doc.rust-lang.org/std/result/enum.Result.html)
  */
-public sealed class Result<out V, out E> {
+public sealed interface Result<out V, out E> {
 
-    public abstract operator fun component1(): V?
-    public abstract operator fun component2(): E?
+    public operator fun component1(): V?
+    public operator fun component2(): E?
 
     public companion object {
 
@@ -32,45 +34,51 @@ public sealed class Result<out V, out E> {
 /**
  * Represents a successful [Result], containing a [value].
  */
-public class Ok<out V>(public val value: V) : Result<V, Nothing>() {
+public interface Ok<out V> : Result<V, Nothing> {
+    public val value: V
+}
 
-    override fun component1(): V = value
+/**
+ * Constructor function for [Ok] as a success representation of [Result]
+ */
+@Suppress("FunctionName")
+public fun <V> Ok(value: V): Ok<V> = InternalOk(value)
+
+@JvmInline
+private value class InternalOk<out V>(private val raw: Any?) : Ok<V> {
+
+    override fun component1(): V? = value
     override fun component2(): Nothing? = null
 
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other == null || this::class != other::class) return false
+    @Suppress("UNCHECKED_CAST")
+    override val value: V
+        get() = raw as V
 
-        other as Ok<*>
-
-        if (value != other.value) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int = value.hashCode()
     override fun toString(): String = "Ok($value)"
 }
 
 /**
  * Represents a failed [Result], containing an [error].
  */
-public class Err<out E>(public val error: E) : Result<Nothing, E>() {
+public interface Err<out E> : Result<Nothing, E> {
+    public val error: E
+}
+
+/**
+ * Constructor function for [Err] as a failure representation of [Result]
+ */
+@Suppress("FunctionName")
+public fun <E> Err(error: E): Err<E> = InternalErr(error)
+
+@JvmInline
+private value class InternalErr<out E>(private val raw: Any?) : Err<E> {
 
     override fun component1(): Nothing? = null
-    override fun component2(): E = error
+    override fun component2(): E? = error
 
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other == null || this::class != other::class) return false
+    @Suppress("UNCHECKED_CAST")
+    override val error: E
+        get() = raw as E
 
-        other as Err<*>
-
-        if (error != other.error) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int = error.hashCode()
     override fun toString(): String = "Err($error)"
 }
